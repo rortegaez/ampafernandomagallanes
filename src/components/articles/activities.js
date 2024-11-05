f4520ca53da9a4c3dcfb394378cd6a68cd587c95
rortegaez <you@example.com>
2024-11-05 17:52:27 +0100

    change activities and news

---
+++ src/components/articles/activities.js
@@ -0,0 +1,37 @@
+const ACTIVITIES = [
+  {
+
+  {
+		ident: "Halloween2024",
+		title: 'Fiesta de Halloween 2024',
+		body: [
+			'Por motivos de la fiesta de Halloween un grupo de mamis, han estado trabajando en para montar una decoración espectacular. Para tener una fiesta, que se había perdido y entre todos, con el apoyo del cole, la vamos a recuperar para el disfrute de los niños.',
+			'Por culpa de las inclemencias del tiempo, nos hemos visto obligados a aplazar la fiesta para el miércoles día 6 de noviembre, a partir de las 17.15h. Sabemos que al día siguiente hay colegio, pero sería espectacular que entre todos hagamos un esfuerzo para llenar nuestro patio y que las familias del cole disfrutéis un rato.',
+			'En la fiesta vais a encontrar una actividad de manualidades para vuestros niños, una barra con bebidas, dulces de Halloween y golosinas, además habrá un puesto de palomitas y algodón de azúcar. Y tendremos un castillo hinchable, del tipo gymkana para el disfrute de todos los niños.',
+			'Os esperamos, será un día de fiesta y disfrute de nuestra familia del cole.',
+			'Un saludo y gracias',
+			'AMPA Fernando Magallanes'
+		],
+		date: '01-11-2024',
+		description: 'activities',
+		image: [
+			"/halloween24/halloween24.jpg",
+		]
+	},
+  {
+		ident: "repartoLeche",
+		title: "Reparto leche con Nesquik",
+		body: [
+			'Desde el colegio nos pidieron que les ayudasemos al reparto de leche con Nesquik para los niños del colegio. Allí estuvimos parte de la directiva del AMPA y varios padres socios, que no dudaron en apuntarse a ayudar.',
+			'Pasamos un rato de risas preparando la mezcla en las botellas de leches, para cuando estuvo todo listo, llevarlo a las diferentes clases juntos con sus vasos. Para que llegada la hora del patio, se encargase los profesores de repartir la leche a cada uno.',
+			'Terminado el "trabajo", nos hicimos la foto que veis a continuación y hasta la próxima vez que el colegio necesite nuestra ayuda.'
+		],
+		description: 'activities',
+		date: '09-04-2024',
+		image: [
+			"/repartoLeche/repartoLeche.jpg"
+		]
+	},
+]
+
+export default ACTIVITIES
