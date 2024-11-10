e7af5bfb452abf4ce7a3fce8b7879093516a5ac8
rortegaez <raulor83@gmail.com>
2024-11-11 00:35:25 +0100

    change css and first companions

---
+++ src/components/articles/companions.js
@@ -0,0 +1,16 @@
+const COMPANIONS = [
+	{
+		name: "Raúl",
+		responsability: "Presidente",
+		photo: "/companios/raul.jpg",
+		text: "El AMPA no esta solo para hacer fiestas."
+	},
+	{
+		name: "Salima",
+		responsability: "Vocal",
+		photo: "/companions/salima.jpg",
+		text: "Vamos viendo"
+	}
+]
+
+export default COMPANIONS
