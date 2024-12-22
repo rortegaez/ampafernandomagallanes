1ec2620f12a00d90e4b770a72f191ea70ce1dd50
rortegaez <you@example.com>
2024-12-22 08:31:05 +0100

    add news, pdte css

--- src/components/page/News.jsx
+++ src/components/page/News.jsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
+
+import ARTICULOS from "../article/articulos.js";
+
+import "./css/news.css";
+
 export const News = () => {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    const recoveryNews = () => {
+      const recoveryArticles = ARTICULOS.filter(
+        (element) => element.description === "news"
+      );
+      setNews(recoveryArticles);
+    };
+    recoveryNews();
+  }, []);
   return (
     <>
       <h1>News</h1>
+      <div>
+        {news.map((items) => (
+          <div key={items.ident}>
+            <h2>{items.title}</h2>
+            {items.body.map((item) => (
+              <p key={item}>{item}</p>
+            ))}
+            <div>
+              {items.image?.map((imgs) => (
+                <img src={imgs} alt="photo" key={imgs} className="imgNews" />
+              ))}
+            </div>
+            <h5>{items.date}</h5>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
